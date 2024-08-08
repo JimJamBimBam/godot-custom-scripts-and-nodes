a9a14bfcdab84f780af717e2c20d7159461bd780
James Sonneveld <jsonneveld2@hotmail.com>
2024-08-08 12:40:17 +1000

    Added new decorator node 'Inverter' which allows for the inversion of action/condition nodes 'status'. Added unit tests for inverter node.

---
+++ addons/behaviour_tree/nodes/decorators/inverter.gd
@@ -0,0 +1,39 @@
+class_name Inverter extends Decorator
+
+## An inverter will return 'FAILURE' when it's child returns 'SUCCESS' or,
+## 'SUCCESS' if the child returns 'FAILURE'.
+## Returns 'RUNNING' if child node is also 'RUNNING'.
+
+func tick(actor: Node, blackboard: Blackboard) -> int:
+	var child = get_child(0) as BTNode
+	
+	if child != running_child:
+		child.before_run(actor, blackboard)
+	
+	var response: int = child.tick(actor, blackboard)
+	
+	if child is ConditionLeaf:
+		blackboard.set_value("last_condition", child, str(actor.get_instance_id()))
+		blackboard.set_value("last_condition_status", response, str(actor.get_instance_id()))
+	
+	match response:
+		SUCCESS:
+			child.after_run(actor, blackboard)
+			return FAILURE
+		FAILURE:
+			child.after_run(actor, blackboard)
+			return SUCCESS
+		RUNNING:
+			running_child = child
+			if child is ActionLeaf:
+				blackboard.set_value('running_action', child, str(actor.get_instance_id()))
+			return RUNNING
+		_:
+			push_error("This should be unreachable")
+			return -1
+
+
+func get_class_name() -> Array[StringName]:
+	var classes := super()
+	classes.push_back(&"Inverter")
+	return classes
---
+++ addons/behaviour_tree/test/nodes/decorators/inverter_test.gd
@@ -0,0 +1,49 @@
+# GdUnit generated TestSuite
+class_name InverterTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source = 'res://addons/behaviour_tree/nodes/decorators/inverter.gd'
+const __mock_action = "res://addons/behaviour_tree/test/nodes/actions/mock_action.gd"
+const __behaviour_tree = "res://addons/behaviour_tree/nodes/behaviour_tree.gd"
+const __blackboard = "res://addons/behaviour_tree/blackboard.gd"
+
+var behaviour_tree: BehaviourTree
+var action: MockAction
+var inverter = Inverter
+
+
+func before_test() -> void:
+	behaviour_tree = auto_free(load(__behaviour_tree).new())
+	action = auto_free(load(__mock_action).new())
+	inverter = auto_free(load(__source).new())
+	
+	var actor = auto_free(Node.new())
+	var blackboard = auto_free(load(__blackboard).new())
+	
+	behaviour_tree.add_child(inverter)
+	inverter.add_child(action)
+	
+	behaviour_tree.actor = actor
+	behaviour_tree.blackboard = blackboard
+
+
+func test_invert_success_to_failure() -> void:
+	action.final_result = BTNode.SUCCESS
+	assert_that(behaviour_tree.tick()).is_equal(BTNode.FAILURE)
+
+
+func test_invert_failure_to_success() -> void:
+	action.final_result = BTNode.FAILURE
+	assert_that(behaviour_tree.tick()).is_equal(BTNode.SUCCESS)
+
+
+func test_clear_running_child_after_run() -> void:
+	action.final_result = BTNode.RUNNING
+	behaviour_tree.tick()
+	assert_that(inverter.running_child).is_equal(action)
+	action.final_result = BTNode.SUCCESS
+	behaviour_tree.tick()
+	assert_that(inverter.running_child).is_null()
