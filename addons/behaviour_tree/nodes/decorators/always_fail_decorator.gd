064172475bd6c431b187fd88a78a756b25e4d528
James Sonneveld <jsonneveld2@hotmail.com>
2024-08-09 21:04:24 +1000

    Added 'always_fail_decorator' which returns a status of 'failure' even when a node succeeds, or 'running' every other time.

---
+++ addons/behaviour_tree/nodes/decorators/always_fail_decorator.gd
@@ -0,0 +1,33 @@
+class_name AlwaysFailDecorator extends Decorator
+
+
+## Always Fail Decorator will always return a 'FAILURE' status
+## whether or not an action node returns 'SUCCEED' or 'FAILURE'.
+## Will, however, return 'RUNNING' if an action node is 'RUNNING'.
+
+func tick(actor: Node, blackboard: Blackboard) -> int:
+	var child: BTNode = get_child(0) as BTNode
+	
+	if child != running_child:
+		child.before_run(actor, blackboard)
+	
+	var response: int = child.tick(actor, blackboard)
+	
+	if child is ConditionLeaf:
+		blackboard.set_value(LAST_CONDITION, child, str(actor.get_instance_id()))
+		blackboard.set_value(LAST_CONDITION_STATUS, response, str(actor.get_instance_id()))
+	
+	if response == RUNNING:
+		running_child = child
+		if child is ActionLeaf:
+			blackboard.set_value(RUNNING_ACTION, child, str(actor.get_instance_id()))
+		return RUNNING
+	else:
+		child.after_run(actor, blackboard)
+		return FAILURE
+
+
+func get_class_name() -> Array[StringName]:
+	var classes := super()
+	classes.push_back(&"AlwaysSucceedDecorator")
+	return classes
